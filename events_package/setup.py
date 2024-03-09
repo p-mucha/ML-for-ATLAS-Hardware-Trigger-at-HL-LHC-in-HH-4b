77f410153aec3e3946e1f4178ad42e600228a0f6
Paweł Mucha <m.pawel2001@gmail.com>
2024-03-09 23:51:53 +0000

    Add files via upload

---
+++ events_package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import re
+
+
+setup(
+    name="events_package",
+    version="5.0",
+    packages=["events_package"],
+    author=["Pawel Mucha"],
+    author_email=["pawel_mucha@zohomail.eu"],
+)
